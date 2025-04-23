d82cf283dcd39321bebd14bf688b56bdd1d0ec4f
simpson <jw0060@xisnd.com>
2025-04-23 10:14:42 +0900

    base src

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import GlobalStyle from "./styles/GlobalStyle";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserStore from "./context/UserStore";
+import Login from "./pages/signup/Login";
+import Signup from "./pages/signup/Signup";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import ChatList from "./pages/chatting/ChatList";
+import ChatRoomCreate from "./pages/chatting/ChatRoomCreate";
+import Chatting from "./pages/chatting/Chatting";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <UserStore>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route element={<Layout />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/chat" element={<ChatList />} />
+              <Route path="/chat-create" element={<ChatRoomCreate />} />
+              <Route path="/chatting/:roomId" element={<Chatting />} />
+            </Route>
+          </Routes>
+        </Router>
+      </UserStore>
+    </>
+  );
+}
+
+export default App;
---
+++ src/api/AxiosApi.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import AxiosInstance from "./AxiosInstance";
+const KH_DOMAIN = "http://localhost:8111";
+
+const AxiosApi = {
+  login: async (email, pw) => {
+    console.log("이메일 : ", email);
+    console.log("비밀번호 : ", pw);
+    const login = {
+      email: email,
+      pwd: pw,
+    };
+    return await axios.post(KH_DOMAIN + "/auth/login", login);
+  },
+  regCheck: async (email) => {
+    return await axios.get(KH_DOMAIN + `/auth/exists/${email}`);
+  },
+  signup: async (email, pw, name) => {
+    const member = {
+      email: email,
+      pwd: pw,
+      name: name,
+      imgPath: "",
+    };
+    return await axios.post(KH_DOMAIN + `/auth/signup`, member);
+  },
+  // memberList: async () => {
+  //   return await axios.get(KH_DOMAIN + "/auth/memberAll");
+  // },
+  // 전체 회원 조회
+  memberList: async () => {
+    // return await axios.get(KH_DOMAIN + "/member/getInfo");
+    return await AxiosInstance.get("/member/getInfo");
+  },
+  // 개별 회원 조회
+  memberInfo: async (email) => {
+    return await AxiosInstance.get(`/member/getInfo`, {
+      params: {
+        email: email,
+      },
+    });
+  },
+  // 채팅방 목록 가져오기
+  chatList: async () => {
+    return await axios.get(KH_DOMAIN + "/chat/list");
+  },
+  // 채팅방 생성하기
+  chatCreate: async (email, name) => {
+    const chat = {
+      email: email,
+      name: name,
+    };
+    return await axios.post(KH_DOMAIN + "/chat/new", chat);
+  },
+  // 채팅방 정보 가져오기
+  chatDetail: async (roomId) => {
+    return await axios.get(KH_DOMAIN + `/chat/room/${roomId}`);
+  },
+};
+
+export default AxiosApi;
